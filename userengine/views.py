f48e08c60a04fb4321f8e8bec719178dff06dfa5
Rafael Adel <rafael.adel20@gmail.com>
2012-10-18 23:52:40 +0200

    Inserted validation function

--- userengine/views.py
+++ userengine/views.py
@@ -6,6 +6,7 @@ from userengine.models import users
 from django.core.exceptions import ObjectDoesNotExist
 import hashlib
 import datetime
+from validation import validate
 
 def start(request):
 	return render_to_response("signin.html", context_instance=RequestContext(request) )
@@ -27,21 +28,29 @@ def signin(request):
 def show_register(request):
 	return render_to_response("register.html", context_instance=RequestContext(request))
 
+
+
 def register(request):
-	if ("username" in request.POST) and ("password" in request.POST) and ("password_again" in request.POST):
-		exists = users.objects.filter(username__exact= request.POST["username"])
-		if exists:
-			return render_to_response("register.html", {"error_message": "User already exists"}, context_instance=RequestContext(request))
-		else:
-			if request.POST["password"] != request.POST["password_again"]:
-				return render_to_response("register.html", {"error_message":"Password confirmation didn't match!"}, context_instance=RequestContext(request))
-			password = hashlib.sha256(request.POST["password"]).hexdigest()
-			new_user = users(username = request.POST["username"],
-							hashed_password = password,
-							email = request.POST.get("email",""),
-							last_login = datetime.datetime.now())
-			new_user.save()
-			return render(request, "signin.html",{"error_message":"New user added."})
+	if not validate(request.POST["username"]):
+		return render_to_response("register.html", {"error_message": "Invalid username."}, context_instance=RequestContext(request))
+	
+	if not validate(request.POST["password"]):
+		return render_to_response("register.html", {"error_message": "Invalid password."}, context_instance=RequestContext(request))
+		
+	if request.POST["password"] != request.POST["password_again"]:
+		return render_to_response("register.html", {"error_message":"Password confirmation didn't match!"}, context_instance=RequestContext(request))
+	
+	exists = users.objects.filter(username__exact= request.POST["username"])
+	if exists:
+		return render_to_response("register.html", {"error_message": "User already exists"}, context_instance=RequestContext(request))
+	else:		
+		password = hashlib.sha256(request.POST["password"]).hexdigest()
+		new_user = users(username = request.POST["username"],
+						hashed_password = password,
+						email = request.POST.get("email",""),
+						last_login = datetime.datetime.now())
+		new_user.save()
+		return render(request, "signin.html",{"error_message":"New user added."})
 		
                 
 
