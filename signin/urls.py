5fea8bd648a4a2c49ae22ab947e8ec4d2e0c1002
Rafael Adel <rafael.adel20@gmail.com>
2012-10-21 17:32:03 +0200

    removed start function importing

--- signin/urls.py
+++ signin/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from userengine.views import start
 admin.autodiscover()
 
-urlpatterns = patterns('',
-	url(r'^$', start),
+urlpatterns = patterns('',	
 	url(r'', include('userengine.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
